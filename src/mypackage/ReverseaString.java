454d9cf37b7e3e90d27a0475ea6cef7b1224a6e4
Vishnu <vishnupriyapera@gmail.com>
2026-04-10 17:58:44 +0530

    second commit

--- src/mypackage/ReverseaString.java
+++ src/mypackage/ReverseaString.java
@@ -4,7 +4,7 @@ public class ReverseaString {
 
 	public static void main(String[] args) {
 		
-		String str = "Vishnu";
+		String str = "Selenium";
 		String rev = "";
 		for(int i= str.length()-1; i>=0; i--) {
 			rev = rev + str.charAt(i);
